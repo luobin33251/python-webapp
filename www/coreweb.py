4f41122a9c0dcd45d8f61d2f145f926bf8bf5d97
luobin1 <1114513874@qq.com>
2017-07-06 15:00:26 +0800

    33251

--- www/coreweb.py
+++ www/coreweb.py
@@ -105,5 +105,59 @@ class RequestHandler(object):
 						kw[k]=v[0]
 		if kw is None:
 			kw=dict(**request.match_info)
+		else:
+			if not self._has_var_kw_arg and self._named_kw_args:
+				copy=dict()
+				for name in self._named_kw_args:
+					if name in kw:
+						copy[name]=kw[name]
+					kw=copy
+			for k,v in request.match_info.items():
+				if k in kw:
+					logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+				kw[k]=v
+		if self._has_request_arg:
+			kw['request']=request
+		if self._required_kw_args:
+			for name in self._required_kw_args:
+				if not name in kw:
+					return web.HTTPBadRequest('Missing argument: %s' % name)
+		logging.info('call with args: %s' % str(kw))
+		try:
+			r=await self._fun(**kw)
+			return r
+		except APIError as e:
+			raise dict(error=e.error, data=e.data, message=e.message)
 		
+def add_static(app):
+	path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'static')
+	app.router.add_static('/static',path)
+	logging.info('add static %s => %s'%('/static/',path))
+
+def add_route(app,fn):
+	method=getattr(fn,'__method__',None)
+	path=getattr(fn,'__route__',None)
+	if path is None or method is None:
+		raise ValueError('@get or @post not define in %s'%str(fn))
+	if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+		fn=asyncio.corotine(fn)
+	logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+	app.router.add_route(method, path, RequestHandler(app, fn))
+
+def add_routes(app,module_name):
+	n=module_name.rfind('.')
+	if n == (-1):
+		mod=__import__(module_name,globals(),locals())
+	else:
+		name=module_name[n+1:]
+		mod=getattr(__import__(module_name[:n],globals(),locals(),[name]),name)
+	for attr in dic(mod):
+		if attr.startsWith('_'):
+			continue
+		fn=getattr(mod,attr)
+		if callable(fn):
+			method=getattr(fn,'__method__',None)
+			path=getattr(fn,'__route__',None)
+			if method and path:
+				add_route(app,fn)
 
